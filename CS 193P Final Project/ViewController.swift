e16af835957139e7257ce8fa2c3efad60fca2a68
Christopher Ponce de Leon <cponcede@rescomp-16-303152.stanford.edu>
2017-02-28 19:18:30 -0800

    Initial Commit

---
+++ CS 193P Final Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CS 193P Final Project
+//
+//  Created by Christopher Ponce de Leon on 2/28/17.
+//  Copyright © 2017 Stanford University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
