4bce75a985c30ea342ce470532616836d720c947
Christopher Ponce de Leon <cponcede@rescomp-16-303152.stanford.edu>
2017-03-14 23:05:30 -0700

    Have chart working but want to change some axis labels

--- CS 193P Final Project/ArtistStatisticsViewController.swift
+++ CS 193P Final Project/ArtistStatisticsViewController.swift
@@ -5,28 +5,18 @@
 //  Created by Christopher Ponce de Leon on 3/12/17.
 //  Copyright © 2017 Stanford University. All rights reserved.
 //
-
+import Charts
 import UIKit
 
-class ArtistStatisticsViewController: UIViewController {
+class ArtistStatisticsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    @IBOutlet weak var graphView: StatsGraphView? {
+    @IBOutlet weak var dataTableView: UITableView!
+    
+    @IBOutlet weak var graphView: BarChartView? {
         didSet { updateUI() }
     }
     
-    // Static dictionary for converting from int representation of month to the name of the month.
-    static let intToMonth = [1: "January",
-                     2: "February",
-                     3: "March",
-                     4: "April",
-                     5: "May",
-                     6: "June",
-                     7: "July",
-                     8: "August",
-                     9: "September",
-                     10: "October",
-                     11: "November",
-                     12: "December"]
+    private var cachedPlaybackData : [String : Int]?
     
     // Get American representatino of date as String
     private static func getDateString(day : Int, month : Int, year : Int) -> String {
@@ -41,28 +31,60 @@ class ArtistStatisticsViewController: UIViewController {
             print("Showing listening stats for \(artist!.name!)")
             for item in (artist?.songs)! {
                 if let songPlayCount = item as? SongPlayCount {
+                    print("\(Int(songPlayCount.month))/\(Int(songPlayCount.day))/\(Int(songPlayCount.year)) - \(Int(songPlayCount.count))")
                     songPlayStatistics.setPlayCountForDate(year: Int(songPlayCount.year),
                                                            month: Int(songPlayCount.month),
                                                            day: Int(songPlayCount.day),
                                                            count: Int(songPlayCount.count))
                 }
             }
-            songPlayStatistics.printStats()
+            //songPlayStatistics.printStats()
             updateUI()            
         }
     }
     
-    private func updateUI() {
-        if graphView != nil && artist != nil {
-            graphView!.songPlayStatistics = self.songPlayStatistics
-        }
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        dataTableView.dataSource = self
+        dataTableView.delegate = self
     }
+    
+    private func updateUI() {
+        if graphView != nil && artist != nil {
+            let (_, yVals, dates) = songPlayStatistics.getGraphStats()
+            var dataEntries : [BarChartDataEntry] = []
+            for i in 0..<yVals.count {
+                let dataEntry = BarChartDataEntry(x: Double(i), y: Double(yVals[i]))
+                dataEntries.append(dataEntry)
+            }
+            let chartDataSet = BarChartDataSet(values: dataEntries, label: "Plays per month")
+            let chartData = BarChartData.init(dataSets: [chartDataSet])
+            graphView!.xAxis.valueFormatter = IndexAxisValueFormatter(values: dates)
+            graphView!.xAxis.granularity = 1
+            graphView!.data = chartData
+            dataTableView.reloadData()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if cachedPlaybackData == nil {
+            cachedPlaybackData = songPlayStatistics.getAllData()
+        }
+        if section == 0  {
+            return cachedPlaybackData!.keys.count
+        }
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "dateCount", for: indexPath)
+        let dateString = cachedPlaybackData!.keys.sorted()[indexPath.row]
+        cell.textLabel?.text = dateString
+        cell.detailTextLabel?.text = String(describing: cachedPlaybackData![dateString]!)
+        return cell
+    }
+
+
     
 
 }
--- CS 193P Final Project/SongPlayStatistics.swift
+++ CS 193P Final Project/SongPlayStatistics.swift
@@ -10,6 +10,33 @@ import Foundation
 
 class SongPlayStatistics {
     
+    private let daysInMonth = [1: 31,
+                               2: 28,
+                               3: 31,
+                               4: 30,
+                               5: 31,
+                               6: 30,
+                               7: 31,
+                               8: 31,
+                               9: 30,
+                               10: 31,
+                               11: 30,
+                               12: 31]
+    
+    // Static dictionary for converting from int representation of month to the name of the month.
+    static let intToMonth = [1: "Jan",
+                             2: "Feb",
+                             3: "Mar",
+                             4: "Apr",
+                             5: "May",
+                             6: "Jun",
+                             7: "Jul",
+                             8: "Aug",
+                             9: "Sep",
+                             10: "Oct",
+                             11: "Nov",
+                             12: "Dec"]
+    
     private var yearlyData = [Int: [Int: [Int: Int]]]()
     
     func setPlayCountForDate(year: Int, month: Int, day: Int, count: Int) {
@@ -30,21 +57,51 @@ class SongPlayStatistics {
         }
     }
     
-    // TODO: Figure out best way to represent play counts to graph using custom view
+    func getGraphStats() -> ([Int], [Int], [String]) {
+        var xVals : [Int] = []
+        var yVals : [Int] = []
+        var dates : [String] = []
+        let sortedYears = yearlyData.keys.sorted()
+        let firstYear = sortedYears[0]
+        let lastYear = sortedYears[sortedYears.count - 1]
+        var monthCount = 0
+        for year in firstYear...lastYear {
+            for month in 1...12 {
+                if yearlyData[year] == nil ||
+                    yearlyData[year]![month] == nil {
+                    xVals.append(monthCount)
+                    yVals.append(0)
+                    dates.append("\(month)/\(year)")
+                } else {
+                    var playbacks = 0
+                    for day in yearlyData[year]![month]!.keys {
+                        playbacks = yearlyData[year]![month]![day]! + playbacks
+                    }
+                    xVals.append(monthCount)
+                    yVals.append(playbacks)
+                    dates.append("\(month)/\(year)")
+                }
+                monthCount = monthCount + 1
+            }
+        }
+        return (xVals, yVals, dates)
+    }
     
     private func getDateString(day : Int, month : Int, year : Int) -> String {
         return "\(month)/\(day)/\(year)"
     }
     
-    func printStats() {
+    func getAllData() -> [String: Int]{
+        var result = [String : Int]()
         for year in yearlyData.keys.sorted() {
             for month in yearlyData[year]!.keys.sorted() {
                 for day in yearlyData[year]![month]!.keys.sorted() {
                     let count = yearlyData[year]![month]![day]!
                     let dateString = getDateString(day: day, month: month, year: year)
-                    print("\(dateString) - \(count)")
+                    result[dateString] = count
                 }
             }
         }
+        return result
     }
 }
